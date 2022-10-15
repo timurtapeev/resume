09d67671d463a66154434f2efde0dc5f6f6db039
tapeevtimur <tapeevtimur@mail.ru>
2022-10-15 16:32:22 +0500

    final commite

---
+++ src/mailer/smart.php
@@ -0,0 +1,36 @@
+<?php 
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$text = $_POST['text'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$mail->isSMTP();
+$mail->Host = 'smtp.mail.ru';
+$mail->SMTPAuth = true;
+$mail->Username = 'timurtimurovich2001@mail.ru';
+$mail->Password = 'XhRaM9WFv6eQ8SwvubC5';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+ 
+$mail->setFrom('timurtimurovich2001@mail.ru', 'Заявка с сайта');
+$mail->addAddress('fiheyol769@deitada.com');     
+$mail->isHTML(true);                                  
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br> 
+	Имя: ' . $name . ' <br>
+	Почта: ' . $email . '<br>
+	Сообщение: ' . $text . '<br>';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
